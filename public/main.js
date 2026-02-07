a0a75d34f911fa24d679990b7facd61e474e05c8
denmelk1yy <densoltys@gmail.com>
2026-02-07 15:53:33 +0200

    add-main.js

--- public/main.js
+++ public/main.js
@@ -1,1 +1,27 @@
- 
+ $('#create').click(() => {
+     let data = {
+         name: $('#name').val(),
+         price: $('#price').val(),
+         category: $('#category').val()
+     };
+     console.log(data);
+     axios.post('http://localhost:3000/api/products', data)
+         .then(res => {
+             console.log(res.data)
+         })
+         .catch(err => console.error(err));
+ })
+
+
+ axios.get('http://localhost:3000/api/products')
+.then(res => {
+   for(let el of res.data){
+       $('.redactContainer').append(`
+       <div class="product">
+       <p>${el.name}</p>
+       <p>${el.price}</p>
+       <p>${el.category}</p>
+       </div>
+       `)
+   }
+})
